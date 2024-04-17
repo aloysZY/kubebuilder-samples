a448fd3bcd030f8a9e5a0ae07f034bc29b00ee66
aloys <aloys.zy@hotmail.com>
2024-04-17 18:08:52 +0800

    添加rbac相关标签

--- internal/controller/app_controller.go
+++ internal/controller/app_controller.go
@@ -57,6 +57,14 @@ type AppReconciler struct {
 // +kubebuilder:rbac:groups=aloys.tech.aloys.tech,resources=apps,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=aloys.tech.aloys.tech,resources=apps/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups=aloys.tech.aloys.tech,resources=apps/finalizers,verbs=update
+// +kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=apps,resources=deployments/status,verbs=get;update
+// +kubebuilder:rbac:groups=core,resources=services,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=core,resources=services/status,verbs=get;update
+// +kubebuilder:rbac:groups=networking.k8s.io,resources=ingresses,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=networking.k8s.io,resources=ingresses/status,verbs=get;update
+// +kubebuilder:rbac:groups=autoscaling,resources=horizontalpodautoscalers,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=autoscaling,resources=horizontalpodautoscalers/status,verbs=get;update
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
